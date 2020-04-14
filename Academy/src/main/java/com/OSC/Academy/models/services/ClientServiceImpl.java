74f0df006e5e8c04eaf804f17e600a7a8c31c627
Membri <membri_2@hotmail.com>
2020-04-14 21:57:30 +0200

    clientController implemented

--- Academy/src/main/java/com/OSC/Academy/models/services/ClientServiceImpl.java
+++ Academy/src/main/java/com/OSC/Academy/models/services/ClientServiceImpl.java
@@ -1,5 +1,29 @@
 package com.OSC.Academy.models.services;
 
-public class ClientServiceImpl {
+import java.util.List;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.OSC.Academy.models.dao.IClientDao;
+import com.OSC.Academy.models.entity.Client;
+
+//this anotation mark this like a service component
+@Service
+public class ClientServiceImpl implements IClientService {
+	
+	//dependencies injector
+	@Autowired
+	private IClientDao clientDao;
+	
+	@Override
+
+	public List<Client> findAll() {
+		// TODO Auto-generated method stub
+		return (List<Client>) clientDao.findAll();
+	}
 
 }
--- Academy/src/main/java/com/OSC/Academy/models/services/IClientService.java
+++ Academy/src/main/java/com/OSC/Academy/models/services/IClientService.java
@@ -1,5 +1,8 @@
 package com.OSC.Academy.models.services;
 
-public interface IClientService {
+import java.util.List;
 
+import com.OSC.Academy.models.entity.*;
+public interface IClientService {
+	public List<Client> findAll();
 }
