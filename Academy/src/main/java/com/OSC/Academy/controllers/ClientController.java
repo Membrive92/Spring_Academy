9783678b46aaaa1363bda0dc13fb4f2cefa72199
Membri <membri_2@hotmail.com>
2020-04-27 19:43:59 +0200

    show, create, update and delete methods implemented in ClientController

--- Academy/src/main/java/com/OSC/Academy/controllers/ClientController.java
+++ Academy/src/main/java/com/OSC/Academy/controllers/ClientController.java
@@ -3,9 +3,16 @@ package com.OSC.Academy.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.OSC.Academy.models.entity.Client;
@@ -23,5 +30,33 @@ public class ClientController {
 	@GetMapping("/clients")
 	public List<Client> index(){
 		return clientService.findAll();
+	}
+	@GetMapping("/clients/{id}")
+	public Client show(@PathVariable Long id) {
+		return clientService.findById(id);
+	}
+	
+	@PostMapping("/clients")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client create(@RequestBody Client client) {
+		return clientService.save(client);
+	}
+	
+	@PutMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client update(@RequestBody Client client, @PathVariable Long id) {
+		 Client currentClient = clientService.findById(id);
+		 
+		 currentClient.setName(client.getName());
+		 currentClient.setLastName(client.getLastName());
+		 currentClient.setEmail(client.getEmail());
+		 
+		 return clientService.save(currentClient);
+	}
+	
+	@DeleteMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clientService.delete(id);
 	}
 }
