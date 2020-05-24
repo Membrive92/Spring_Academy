ec6d7649a73c43b4e808c48fbde0d393e9b78f74
Membri <membri_2@hotmail.com>
2020-05-24 12:34:23 +0200

    upload and download image method

--- Academy/src/main/java/com/OSC/Academy/controllers/ClientController.java
+++ Academy/src/main/java/com/OSC/Academy/controllers/ClientController.java
@@ -2,6 +2,7 @@ package com.OSC.Academy.controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,10 +16,13 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -214,11 +218,30 @@ public class ClientController {
 			client.setImage(fileName);
 			clientService.save(client);
 			
-			response.put("cliente", client);
+			response.put("client", client);
 			response.put("message", "Se ha subido correctamente la imagen: " +fileName);
 			
 		}
 		
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	@GetMapping("/uploads/img/{nameImage:.+}")
+	public  ResponseEntity<Resource> viewImage(@PathVariable String nameImage){
+		Path fileRoute = Paths.get("uploads").resolve(nameImage).toAbsolutePath();
+		Resource resource = null;
+
+		try {
+			resource = new UrlResource(fileRoute.toUri());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		if(!resource.exists() && !resource.isReadable()){
+			throw new RuntimeException("Error no se pudo cargar la imagen: " + nameImage);
+		}
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"");
+
+		return new ResponseEntity<Resource>(resource,header, HttpStatus.OK);
 	}
 }
